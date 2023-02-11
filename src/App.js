12c66b31cfef68bce28307323ae6a5c2061337bf
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-11 15:12:02 +0300

    routeler yapildi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Form from "./components/Form";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <h1>Teknolojik Yemekler</h1>
-      <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
-    </>
+    <BrowserRouter>
+        <Route exact path="/">
+        <h1>Teknolojik Yemekler</h1>
+        <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
+        </Route>
+        <Route exact path="/pizza">
+        <Form/>
+        </Route>
+
+    </BrowserRouter>
   );
 };
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+
+export default function FormData() {
+
+    return (
+        <section>
+            <div className="formHeader">
+                <h2>Techno Eats</h2>
+                <div>
+                    <button>
+                        Home
+                    </button>
+                    <button>
+                        Help
+                    </button>
+                </div>
+
+            </div>
+            <div className="formBody">
+                <div>
+                    <h3>Build Your Own Pizza</h3>
+                </div>
+                <div>
+                    <form>
+
+
+
+
+
+
+
+                    </form>
+                </div>
+            </div>
+        </section>
+    )
+}
