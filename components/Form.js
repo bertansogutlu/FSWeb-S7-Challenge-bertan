73832958fe0a7ae0474c3539386412db7bc98e5c
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-11 14:38:30 +0300

    Proje kuruldu

---
+++ components/Form.js
@@ -0,0 +1,24 @@
+
+
+
+export default FormData() {
+
+    return (
+        <div>
+            <div>
+                <h2>Build Your Own Pizza</h2>
+            </div>
+            <div>
+                <form>
+
+
+
+
+
+
+
+                </form>
+            </div>
+        </div>
+    )
+}
