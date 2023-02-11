1da65c0a950fb59c06b562342b2762394866b90d
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-12 01:19:42 +0300

    v1

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,91 +12,94 @@ export default function FormData() {
             <div className="formHeader">
                 <h2>Techno Eats</h2>
                 <div>
-                    <button>
+                    <button className="button">
                         Home
                     </button>
-                    <button>
+                    <button className="button">
                         Help
                     </button>
                 </div>
-
             </div>
-
-            <div className="formBody">
-
+            <form className="formBody">
                 <h3 className="center padding bold">Kendi pizzanızı yapın</h3>
                 <img src={pizzaImg} />
                 <h3 className="padding bold">Kendi pizzanızı yapın</h3>
+                <div>
+                    <div className="grey">
+                        <h3 className="padding bold">Boyut seçimi</h3>
+                        <p className="padding">Gerekli</p>
+                    </div>
+                    <input className="margin"></input>
+                </div>
 
-                <form>
-                    <div>
-                        <div className="grey">
-                            <h3 className="padding bold">Boyut seçimi</h3>
-                            <p className="padding">Gerekli</p>
-                        </div>
-                        <input className="margin"></input>
+                <div>
+                    <div className="grey">
+                        <h3 className="padding bold">Sos seçimi</h3>
+                        <p className="padding">Gerekli</p>
                     </div>
 
                     <div>
-                        <div className="grey">
-                            <h3 className="padding bold">Sos seçimi</h3>
-                            <p className="padding">Gerekli</p>
-                        </div>
+                        {soslar.map((e, i) =>
+                            <div>
+                                <label>
+                                    <input type="radio" id={`radio-${i}`} name={e} className="margin"></input>
+                                    {e}
+                                </label>
+                            </div>
+                        )}
+                    </div>
+                </div>
 
-                        <div>
-                            {soslar.map((e, i) =>
-                                <div>
-                                    <label>
-                                        <input type="radio" id={`radio-${i}`} name={e} className="margin"></input>
-                                        {e}
-                                    </label>
-                                </div>
-                            )}
-                        </div>
+                <div>
+                    <div className="grey">
+                        <h3 className="padding bold">Malzeme seçimi</h3>
+                        <p className="padding">10 malzemeye kadar</p>
                     </div>
 
-                    <div>
-                        <div className="grey">
-                            <h3 className="padding bold">Malzeme seçimi</h3>
-                            <p className="padding">10 malzemeye kadar</p>
-                        </div>
+                    <div className="flex">
+                        {malzemeler.map((e, i) =>
+                            <div className="half">
+                                <label>
+                                    <input type="checkbox" className="margin" id={`radio-${i}`} name={e}></input>
+                                    {e}
+                                </label>
+                            </div>
+                        )}
+                    </div>
+                </div>
 
-                        <div className="flex">
-                            {malzemeler.map((e, i) =>
-                                <div className="half">
-                                    <label>
-                                        <input type="checkbox" className="margin" id={`radio-${i}`} name={e}></input>
-                                        {e}
-                                    </label>
-                                </div>
-                            )}
-                        </div>
+                <div>
+                    <div className="grey">
+                        <h3 className="padding bold">Sağlınıza önem veriyoruz</h3>
                     </div>
 
-                    <div>
-                        <div className="grey">
-                            <h3 className="padding bold">Sağlınıza önem veriyoruz</h3>
-                        </div>
+                    <label>
+                        <input type="checkbox" className="margin" id="" name=""></input>
+                        Gulutensiz Un
+                    </label>
+                </div>
 
-                        <label>
-                            <input type="checkbox" className="margin" id="" name=""></input>
-                            Gulutensiz Un
-                        </label>
+                <div>
+                    <div className="grey">
+                        <h3 className="padding bold">Özel bir isteğiniz var mı?</h3>
                     </div>
 
-                    <div>
-                        <div className="grey">
-                            <h3 className="padding bold">Özel bir isteğiniz var mı?</h3>
-                        </div>
+                    <label>
+                        <textarea className="box" name=""></textarea>
+                    </label>
+                </div>
 
-                        <label>
-                            <textarea className="box" rid="" name=""></textarea>
-                        </label>
-                    </div>
+                <div>
+                    <label>
+                        <input type="number" min="1" max="10" className="margin num"></input>
+                    </label>
 
-                </form>
+                    <label>
+                        <button  className="margin" id="" name="">Siparişi tamamla</button>
+                    </label>
+                </div>
 
-            </div>
+            </form>
         </section>
     )
 }
