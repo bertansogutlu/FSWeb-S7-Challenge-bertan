8c65989ec301848448eaf794f0258e54ce91e95e
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-12 14:42:56 +0300

    v10

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,14 @@
 import React from "react"
 import pizzaImg from "../img/Pizza.jpg"
 
-const soslar = ['Pizza sosu', 'Sarımsak sos', 'Meksika acısı', 'Ege zeytinyağı']
-const malzemeler = ['Kaşar', 'Mozzarella', 'Mantar', 'Mısır', 'Biber', 'Zeytin', 'Jalapeño', 'Salam', 'Sucuk', 'Pepperoni']
+const restoranlar = ["McDonals's", "Dominos", "Burger King", "Pizza Hot", "Kentucky Fried Chicken", "Starbucks"]
 
 export default function Main() {
 
     return (
         <section className="main">
             <div className="mainHeader">
-                <h2>Techno Eats</h2>
+                <h2>Techno</h2>
                 <div>
                     <button className="button">
                         Home
@@ -19,9 +18,23 @@ export default function Main() {
                     </button>
                 </div>
             </div>
+
             <div className="mainPhoto">
                 <img src={pizzaImg} />
             </div>
+
+            <div className="mainBody">
+            {restoranlar.map((e, i) =>
+                            <div>
+                                <img src={`https://picsum.photos/id/${i}/400/300`} className="margin"/>
+                                <div>
+                                <h3 className="margin bold">{e}</h3>
+                                <button id={`button-${i}`} name="sos" className="margin">Restorana git</button>
+                                </div>
+                            </div>
+                        )}
+            </div>
+
         </section>
     )
 }
