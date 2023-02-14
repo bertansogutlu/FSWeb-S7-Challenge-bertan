7c38e9102fadcaf18144bec5f86891025626753b
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-14 22:34:54 +0300

    sparis ekrani css yaildi

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -11,11 +11,19 @@ export default function Confirmation({ siparisData }) {
                 <img src={pizzaImg} alt="pizza fotoğrafı" />
                 <div class="centered">Tebrikler {siparisData.isim}! Pizzan yola çıktı</div>
             </div>
-            <div>
+            <div className="siparis">
+                <p className="bold margin">
                 Sipariş adedi: {siparisData.adet}
+                </p>
+                <p className="bold margin">
                 Pizza boyutu: {siparisData.boyut}
+                </p>
+                <p className="bold margin">
                 Pizza sosu: {siparisData.sos}
+                </p>
+                <p className="bold margin">
                 Özel istek: {siparisData["özel"]}
+                </p>
             </div>
         </section>
     )
